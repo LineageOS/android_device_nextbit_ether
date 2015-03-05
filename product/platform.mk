86e7967665c84ea6046b1bcae794e3e5f0379266
Lorenzo Colitti <lorenzo@google.com>
2015-03-05 16:05:23 +0900

    Remove the Ethernet configuration
    This is no longer necessary now that the Ethernet service is
    started on all devices that support USB OTG.
    
    Change-Id: I4f28c5cdd790d76eb8c2c90be689cbc9b93e4ddd

--- product/platform.mk
+++ product/platform.mk
@@ -5,7 +5,5 @@ PRODUCT_PACKAGES += \
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml 
-
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
