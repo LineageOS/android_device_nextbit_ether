a9bf40f0f313bef3dfdcde79112cfe91f0937760
Willi Ye <williye97@gmail.com>
2015-09-13 12:31:06 +0200

    Add Vibrator HW
    Change-Id: I7db8c073730a917eaadeba43f55ab7474f04cb6f

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,7 +1,8 @@
 # CM Hardware
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw
+    hardware/cyanogen/cmhw \
+    $(DEVICE_PATH)/cmhw
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
