7101f0e12e7ab8ea17f755049d242c48129ee496
Steve Kondik <steve@cyngn.com>
2016-04-29 19:09:05 -0700

    ether: Remove extraneous blob
    Change-Id: I2dc95d2574ee6677adfb30a9d0a07e226716a1db

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -92,7 +92,6 @@ EOF
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \\
     CNEService \\
-    DMAgent \\
     ims \\
     imssettings \\
     RCSBootstraputil \\
@@ -202,16 +201,6 @@ LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
 LOCAL_PROPRIETARY_MODULE := true
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := DMAgent
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/app/DMAgent/DMAgent.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
