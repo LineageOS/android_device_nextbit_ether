3d93e051bf540f96dc8f4007519fddd58f9ecd09
Steve Kondik <steve@cyngn.com>
2016-05-12 22:29:29 -0400

    ether: Don't use the codename as the BT device name
     * It's the Nextbit Robin, not the Nextbit Ether :)
    
    Change-Id: I238dd5458fc72cda10cca493445305ed8c5a8e04

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Nextbit Ether"
+#define BTM_DEF_LOCAL_NAME   "Nextbit Robin"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
