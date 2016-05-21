d72f21ce107ad84448fe63f1f5fc81fc44ccebb6
Steve Kondik <steve@cyngn.com>
2016-05-21 00:00:03 -0700

    ether: Copy modem configurations to the target
     * The original path to the SW MBNs in the firmware image was wrong,
       as is the permissions on the direction. Fix it so we can
       enable autoselection.
    
    Change-Id: I7e3bb7ab1e1132865632830befbd79aab3f0d638

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -1,23 +1,26 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    rild.libargs=-d /dev/smd0 \
+    rild.libargs="-d /dev/smd0" \
     ril.subscription.types=NV,RUIM
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.cne.feature=1 \
     persist.data.mode=concurrent \
     persist.data.netmgrd.qos.enable=true \
     persist.data.tcp_rst_drop=true \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.app_hw_mbn_path=/firmware/image/modem_pr/mcfg/configs/mcfg_hw/generic/common/MSM8994/LA \
     persist.radio.custom_ecc=1 \
     persist.radio.sib16_support=1 \
-    ro.use_data_netmgrd=true \
+    persist.radio.hw_mbn_update=1 \
+    persist.radio.sw_mbn_update=1 \
+    persist.radio.sw_mbn_volte=1 \
     ro.data.large_tcp_window_size=true \
+    ro.use_data_netmgrd=true \
     ro.telephony.default_network=9 \
     telephony.lteOnCdmaDevice=1
 
+#PRODUCT_COPY_FILES += \
+#   frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -26,12 +26,6 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-target=`getprop ro.board.platform`
-if [ -f /sys/devices/soc0/soc_id ]; then
-    platformid=`cat /sys/devices/soc0/soc_id`
-else
-    platformid=`cat /sys/devices/system/soc/soc0/id`
-fi
 #
 # Function to start sensors for DSPS enabled platforms
 #
@@ -49,51 +43,6 @@ start_sensors()
     fi
 }
 
-start_battery_monitor()
-{
-	if ls /sys/bus/spmi/devices/qpnp-bms-*/fcc_data ; then
-		chown -h root.system /sys/module/pm8921_bms/parameters/*
-		chown -h root.system /sys/module/qpnp_bms/parameters/*
-		chown -h root.system /sys/bus/spmi/devices/qpnp-bms-*/fcc_data
-		chown -h root.system /sys/bus/spmi/devices/qpnp-bms-*/fcc_temp
-		chown -h root.system /sys/bus/spmi/devices/qpnp-bms-*/fcc_chgcyl
-		chmod 0660 /sys/module/qpnp_bms/parameters/*
-		chmod 0660 /sys/module/pm8921_bms/parameters/*
-		mkdir -p /data/bms
-		chown -h root.system /data/bms
-		chmod 0770 /data/bms
-		start battery_monitor
-	fi
-}
-
-start_charger_monitor()
-{
-	if ls /sys/module/qpnp_charger/parameters/charger_monitor; then
-		chown -h root.system /sys/module/qpnp_charger/parameters/*
-		chown -h root.system /sys/class/power_supply/battery/input_current_max
-		chown -h root.system /sys/class/power_supply/battery/input_current_trim
-		chown -h root.system /sys/class/power_supply/battery/input_current_settled
-		chown -h root.system /sys/class/power_supply/battery/voltage_min
-		chmod 0664 /sys/class/power_supply/battery/input_current_max
-		chmod 0664 /sys/class/power_supply/battery/input_current_trim
-		chmod 0664 /sys/class/power_supply/battery/input_current_settled
-		chmod 0664 /sys/class/power_supply/battery/voltage_min
-		chmod 0664 /sys/module/qpnp_charger/parameters/charger_monitor
-		start charger_monitor
-	fi
-}
-
-start_vm_bms()
-{
-	if [ -e /dev/vm_bms ]; then
-		chown -h root.system /sys/class/power_supply/bms/current_now
-		chown -h root.system /sys/class/power_supply/bms/voltage_ocv
-		chmod 0664 /sys/class/power_supply/bms/current_now
-		chmod 0664 /sys/class/power_supply/bms/voltage_ocv
-		start vm_bms
-	fi
-}
-
 start_copying_prebuilt_qcril_db()
 {
     if [ -f /system/vendor/qcril.db -a ! -f /data/misc/radio/qcril.db ]; then
@@ -101,8 +50,6 @@ start_copying_prebuilt_qcril_db()
         chown -h radio.radio /data/misc/radio/qcril.db
     fi
 }
-
-baseband=`getprop ro.baseband`
 
 case "$baseband" in
         "svlte2a")
@@ -113,23 +60,12 @@ esac
 start_sensors
 start_copying_prebuilt_qcril_db
 
-bootmode=`getprop ro.bootmode`
-emmc_boot=`getprop ro.boot.emmc`
-case "$emmc_boot"
-    in "true")
-        if [ "$bootmode" != "charger" ]; then # start rmt_storage and rfs_access
-            start rmt_storage
-            start rfs_access
-        fi
-    ;;
-esac
-
 #
 # Make modem config folder and copy firmware config to that folder
 #
 rm -rf /data/misc/radio/modem_config
 mkdir /data/misc/radio/modem_config
-chmod 660 /data/misc/radio/modem_config
-cp -r /firmware/image/modem_pr/mbn_ota/* /data/misc/radio/modem_config
+chmod 770 /data/misc/radio/modem_config
+cp -r /firmware/image/mdm/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
 chown -hR radio.radio /data/misc/radio/modem_config
 echo 1 > /data/misc/radio/copy_complete
