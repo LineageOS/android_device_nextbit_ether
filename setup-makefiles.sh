3585a9bd391d5c4e1ca9516958eb46497259c647
Steve Kondik <steve@cyngn.com>
2016-05-22 19:52:13 -0700

    ether: Add blobs for IPv6 tethering
    Change-Id: Ie236f70a32eb471d443f4a6678ba5696954c173d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -98,6 +98,7 @@ PRODUCT_PACKAGES += \\
     ims \\
     imssettings \\
     QtiTelephonyService \\
+    QtiTetherService \\
     shutdownlistener \\
     TimeService
 
@@ -240,6 +241,18 @@ LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := QtiTetherService
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/QtiTetherService/QtiTetherService.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := shutdownlistener
