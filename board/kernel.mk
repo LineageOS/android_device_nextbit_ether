78c1b263cb533373bd14cbe77a41d9166b1ac990
Jake Whatley <jacob.whatley91@gmail.com>
2016-05-24 14:19:49 +0200

    kernel: ether supports appended dt
    ether supports in-kernel appended dt images
    
    dtbTool is unnecessary with this new kernel format
    
    Change-Id: I2f498bebe3763d8d3007e0e6ccf103f31328e7ec

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,7 +3,6 @@ BOARD_KERNEL_PAGESIZE :=  4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 
-BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/nextbit/msm8992
 TARGET_KERNEL_CONFIG := cyanogenmod_ether_defconfig
 TARGET_KERNEL_HEADER_ARCH := arm64
@@ -13,7 +12,6 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 #BOARD_KERNEL_CMDLINE += zcache.enabled=1 zcache.compressor=lz4
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_APPEND_DTB := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
 
 ENABLE_CPUSETS := true
