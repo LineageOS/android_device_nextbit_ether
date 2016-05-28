f2473703f9a5e421aa6b8bf065156d358040380c
Alex Naidis <alex.naidis@linux.com>
2016-05-28 12:31:46 +0200

    ether: gps: use Android Settings as provider for AGPS
    * As per BSP, the value 0 is "Recommended for everybody who use Google SUPL servers."
    * We use Google SUPL servers, so we should follow that recommendation
    
    For reference this information is avaliable here:
    Proprietary_BSP_PATH/gps/Android.mk
    
    Change-Id: I4ed152a0fa443fd1ffa28b0c81bb2a071a38e590
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -10,7 +10,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=0 \
-    ro.gps.agps_provider=1
+    ro.gps.agps_provider=0
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
