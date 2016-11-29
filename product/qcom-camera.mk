d1eec8919f7818fa7451446e97bcd8c4b1d2b9c0
chrmhoffmann <chrmhoffmann@gmail.com>
2016-11-30 00:07:43 +0100

    ether: camera hax
    Change-Id: Iacb5613ef78fbfe542012952db85051f922b0ba9

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,4 +1,8 @@
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+
 PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true \
     persist.camera.cpp.duplication=false \
     ro.qc.sdk.camera.facialproc=false \
     ro.qc.sdk.gestures.camera=false
