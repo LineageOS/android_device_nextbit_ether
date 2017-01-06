389aa5336a4c4800ed62dd5d1ec895df3e9e58b0
chrmhoffmann <chrmhoffmann@gmail.com>
2017-01-06 15:03:09 +0100

    ether: remove camera HALv1 hax
    Change-Id: Ia6219d3defff14435b3842ef5c37174ddd4cfd53

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,6 +1,4 @@
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true \
     persist.camera.cpp.duplication=false \
     ro.qc.sdk.camera.facialproc=false \
     ro.qc.sdk.gestures.camera=false
