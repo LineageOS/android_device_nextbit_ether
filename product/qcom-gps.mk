2f04096cf89d71d10fdc7c6ba2d58b17aa63f3c5
Christopher R. Palmer <crpalmer@gmail.com>
2017-04-08 07:17:33 -0400

    ether: gps: Add xtwifi.conf
    Change-Id: I20fccc763d22c21f7f654c5f40efb91e2ae1018e

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -5,7 +5,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/xtwifi.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=0 \
