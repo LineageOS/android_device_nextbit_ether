4e5e550374d8670954f7fc345a53d20673affe76
Christopher R. Palmer <crpalmer@gmail.com>
2017-04-09 06:33:41 -0400

    ether: bluetooth: Clean up the config
    Remove defines which do no exist and which simply set the default
    value.
    
    Remove BT_CLEAN_TURN_ON_DISABLED which, even though this was the
    QC recommended config, we really shouldn't need.
    
    Change-Id: I2ac49a0f8c3f261faa16519b93b1fc184c952c23

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
  * Copyright (C) 2012 The CyanogenMod Project <http://www.cyanogenmod.org>
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,16 +19,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Nextbit Robin"
-
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    7
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BTM_DEF_LOCAL_NAME      "Nextbit Robin"
+#define BLE_VND_INCLUDED        TRUE
+#define BLUETOOTH_QTI_SW        TRUE
+#define MAX_ACL_CONNECTIONS     7
 
 #endif
