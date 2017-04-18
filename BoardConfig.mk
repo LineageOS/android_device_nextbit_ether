924ce2619fee6567826fb67deba757d90226b566
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    ether: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,4 +22,5 @@ TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 include $(DEVICE_PATH)/PlatformConfig.mk
 include $(DEVICE_PATH)/board/*.mk
 
--include vendor/nextbit/ether/BoardConfigVendor.mk
+# Inherit from the proprietary version
+include vendor/nextbit/ether/BoardConfigVendor.mk
