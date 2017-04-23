5248b0efbc87ead9dd1bb9cad10b427e9fddf24e
Michael Bestas <mikeioannina@gmail.com>
2017-04-23 18:27:41 +0300

    ether: Cleanup kernel config
    * Remove TARGET_KERNEL_HEADER_ARCH, same as TARGET_KERNEL_ARCH
    * Remove cross-compile prefix, set automatically now to
      proper androidkernel toolchain
    * Disable serial console in cmdline
    * Remove zcache cmdline leftovers
    
    Change-Id: I5dadecc48e09501fe0a4cdc1aff92e6d3cd82c0f

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,11 +5,8 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 
 TARGET_KERNEL_SOURCE := kernel/nextbit/msm8992
 TARGET_KERNEL_CONFIG := lineageos_ether_defconfig
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_ARCH := arm64
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
-#BOARD_KERNEL_CMDLINE += zcache.enabled=1 zcache.compressor=lz4
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 ENABLE_CPUSETS := true
