054a86dfab066601371e1b5193a3b7d8e2936716
Michael Bestas <mkbestas@lineageos.org>
2017-06-04 19:41:59 +0300

    ether: gps: Switch to prebuilt HAL
    Change-Id: Ie1c4da361c766ff9b3c6c979011e8a9de06cf0c9

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,12 +1,9 @@
-PRODUCT_PACKAGES += \
-    gps.msm8992
-
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/xtwifi.conf:system/etc/xtwifi.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=0 \
