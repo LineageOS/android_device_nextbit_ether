5b172b3553627df696a096b4666748facac7db47
Paul Keith <javelinanddart@gmail.com>
2017-06-18 01:47:57 +0200

    ether: Use bullhead GPS stack
    Change-Id: I5166f894927aa7e398ebf3aa64e4c3dd426eeeed

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,9 +1,12 @@
+PRODUCT_PACKAGES += \
+    gps.msm8992
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/lowi.conf:system/etc/lowi.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=0 \
