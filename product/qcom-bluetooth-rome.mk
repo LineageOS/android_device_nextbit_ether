23a198e2a215831dbdddc0aed73cd00157c4bca6
Luca Stefani <luca020400@lineageos.org>
2017-06-18 16:14:33 +0200

    ether: Remove deprecated BT props
    Change-Id: I7543cb0272039612d2959333610973c81ab68fa6

--- product/qcom-bluetooth-rome.mk
+++ product/qcom-bluetooth-rome.mk
@@ -3,18 +3,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.enable_timeout_ms=12000 \
-    bluetooth.hfp.client=1 \
     qcom.bluetooth.soc=rome \
     ro.bluetooth.hfp.ver=1.7 \
     ro.bluetooth.dun=true \
-    ro.bluetooth.sap=true \
-    ro.qualcomm.bluetooth.ftp=true \
-    ro.qualcomm.bluetooth.hfp=true \
-    ro.qualcomm.bluetooth.hsp=true \
-    ro.qualcomm.bluetooth.map=true \
-    ro.qualcomm.bluetooth.nap=true \
-    ro.qualcomm.bluetooth.opp=true \
-    ro.qualcomm.bluetooth.pbap=true
+    ro.bluetooth.sap=true
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
