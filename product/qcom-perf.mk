65ec77a41b3daaec68396b5de6196b111c6b60b2
Paul Keith <javelinanddart@gmail.com>
2017-07-11 17:21:34 -0500

    ether: Configure core_ctl
    Change-Id: Iee1eb5cdebbb3e14cd9f5965d9a06917632f4d26

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -3,4 +3,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.at_library=libqti-at.so \
     ro.vendor.gt_library=libqti-gt.so \
-    sys.games.gt.prof=0
+    sys.games.gt.prof=0 \
+    ro.core_ctl_min_cpu=1 \
+    ro.core_ctl_max_cpu=2
