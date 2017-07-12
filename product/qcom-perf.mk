f654e24f51e16f38182ed556841a4149a79add75
Paul Keith <javelinanddart@gmail.com>
2017-07-12 17:03:53 -0500

    ether: Configure core_ctl
    Change-Id: Iee1eb5cdebbb3e14cd9f5965d9a06917632f4d26

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -1,3 +1,5 @@
 # property for vendor specific library
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.core_ctl_min_cpu=0 \
+    ro.core_ctl_max_cpu=2
