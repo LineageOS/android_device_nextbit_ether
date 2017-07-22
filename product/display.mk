7734389139bad4381b0b93ce690c615a23ff88d7
Paul Keith <javelinanddart@gmail.com>
2017-07-21 19:01:29 -0500

    ether: Enable freeform windows
    Change-Id: Ibdcdcc56aba67206ce39be3673a91fd6c71d9b64

--- product/display.mk
+++ product/display.mk
@@ -1,3 +1,6 @@
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
