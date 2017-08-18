dd535c5ad0706529c86262fa38fa057594f15059
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    ether: init: Update for O
    Change-Id: I60475e3dbf68329a46c421e8c8465a4ea2e28203

--- init/init_ether.cpp
+++ init/init_ether.cpp
@@ -30,12 +30,12 @@
 #include <cstdio>
 
 #include <android-base/strings.h>
+#include <android-base/properties.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+#include "vendor_init.h"
 
+using android::base::GetProperty;
 using android::base::Trim;
 
 static void init_alarm_boot_properties()
@@ -44,7 +44,7 @@ static void init_alarm_boot_properties()
     char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
     std::string boot_reason;
     std::string power_off_alarm;
-    std::string tmp = property_get("ro.boot.alarmboot");
+    std::string tmp = GetProperty("ro.boot.alarmboot", "");
 
     if (read_file(boot_reason_file, &boot_reason)
             && read_file(power_off_alarm_file, &power_off_alarm)) {
