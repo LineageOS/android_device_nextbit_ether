540a477bd173d623f5bde634c78499c08388028d
Paul Keith <javelinanddart@gmail.com>
2017-08-22 16:53:10 +0200

    ether: Use HAL1/API1 only for camera
    Change-Id: Ieed3a0a4ec09d133dfe17dc681b2aec91867bfab

--- product/camera.mk
+++ product/camera.mk
@@ -1,12 +1,6 @@
 PRODUCT_PACKAGES += \
     Snap
 
-# Camera2 API
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=1
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
-    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
