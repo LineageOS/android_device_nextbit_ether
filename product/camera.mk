a2f0eb1a05a6f8bc4cc64a1dff05f903f197020a
Paul Keith <javelinanddart@gmail.com>
2017-08-31 10:01:40 -0500

    Revert "ether: Use HAL1/API1 only for camera"
    * While this fixes issues with ZSL and slow auto focus,
      this also broke touch to focus and non zsl usecases
    * TODO: Revisit at a later point in time
    
    This reverts commit ddcdce0d7ba29d3f96f3a4e9c89ac29ad2b4d9c5.
    
    Change-Id: I4d50eba807a30c6a87aa849b2271e13855d49fff

--- product/camera.mk
+++ product/camera.mk
@@ -3,8 +3,10 @@ PRODUCT_PACKAGES += \
 
 # Camera2 API
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=0
+    persist.camera.HAL3.enabled=1
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
