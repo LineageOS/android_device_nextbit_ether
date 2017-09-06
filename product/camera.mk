96d680042c394130fe5cc3df4129ca4a9aa5a205
Michael Bestas <mkbestas@lineageos.org>
2017-09-06 20:30:35 +0300

    ether: Roll back all recent camera changes
    * While this fixes a few bugs, it breaks flashlight.
      We should attempt fixing these issues in Snap instead.
    
    Revert "ether: camera: Force AE bracket off unless in HDR mode"
    
    This reverts commit 975883e275186422a4666491fca07127a73b846c.
    
    Revert "ether: camera: Force ZSL on"
    
    This reverts commit 27cf8acdbfdb79527ef0d573914af8ffe987e33f.
    
    Revert "ether: Add empty camera wrapper"
    
    This reverts commit 88ab325347a1081a93d841cf12e6b30b9601a153.
    
    Revert "ether: Use HAL1/API1 only for camera"
    
    This reverts commit ddcdce0d7ba29d3f96f3a4e9c89ac29ad2b4d9c5.
    
    Change-Id: I82c222e71270ae0f0ab2675337d4fd5adde82405

--- product/camera.mk
+++ product/camera.mk
@@ -1,11 +1,12 @@
 PRODUCT_PACKAGES += \
-    camera.msm8992 \
     Snap
 
 # Camera2 API
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=0
+    persist.camera.HAL3.enabled=1
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
