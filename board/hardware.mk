a2a78dc8f9dec5601a3f691f9e883555ca0a4def
Michael Bestas <mkbestas@lineageos.org>
2018-01-08 18:46:47 +0200

    ether: Remove fragments
    * The experiment didn't really succeed
    
    Change-Id: I9b8a37396cec1fc9881b4b069d29d03434b6c7ad

--- board/hardware.mk
+++
@@ -1,7 +0,0 @@
-# CM Hardware
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
--- product/frp.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/config
