b38ecd1354869abe8f1be5ac2da308ff9d4b7c72
Paul Keith <javelinanddart@gmail.com>
2018-01-22 18:20:55 +0100

    ether: Implement custom power hal lib
    * We'll provide our own tuning
    * Disable core_ctl props while we're at it, they do nothing
    
    Change-Id: I1934a379a221dc877d048124814e0e36ef285690

---
+++ power/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := device/qcom/common/power
+LOCAL_CFLAGS := -Wall -Werror
+LOCAL_SRC_FILES := power-ether.c
+LOCAL_MODULE := libpower_ether
+include $(BUILD_STATIC_LIBRARY)
