d952cdff7e31cd828f01aeb9ab966ec7b1a9dd92
Paul Keith <javelinanddart@gmail.com>
2018-03-06 03:55:36 +0100

    ether: Fix sensor HAL configuration
    * We currently build the hidl multihal, but the conf is in /system,
      we still build the legacy hal, and the flag is still present
    
    Change-Id: I236069e1bc0ba9435a69c2e26b3de5329f083f5b

--- device.mk
+++ device.mk
@@ -254,8 +254,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    sensors.msm8992
+    android.hardware.sensors@1.0-impl
 
 # Thermal
 PRODUCT_COPY_FILES += \
