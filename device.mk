2e8d0e3e02895c28db8512c37fcaabefcb03aedf
Paul Keith <javelinanddart@gmail.com>
2018-03-06 04:00:21 +0100

    ether: Build fingerprint 2.0 service
    * Fixes fingerprint
    
    Change-Id: Ia9d54c08a898e0e862b4bfa090036c8ec5da843e

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
