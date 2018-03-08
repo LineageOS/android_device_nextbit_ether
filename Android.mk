a48e8a1c9200abc662d01d8eac491cbe8e98aca1
Paul Keith <javelinanddart@gmail.com>
2018-03-08 02:41:25 +0100

    ether: Remove symlinks for wlan_mac
    * qcacld can't read this properly, probably because it's
      on a virtual kernel filesystem (procfs)
    * Instead, we'll make the driver read it directly
    
    Change-Id: I856ea0855b3dd53eeaf50092079b055747b5d3b4

--- Android.mk
+++ Android.mk
@@ -73,14 +73,7 @@ $(WCNSS_CFG_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /system/etc/wifi/$(notdir $@) $@
 
-WCNSS_MAC_SYMLINK := $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin
-$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "WCNSS MAC bin link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /proc/wifi_mac $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CFG_SYMLINKS) $(WCNSS_MAC_SYMLINK)
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CFG_SYMLINKS)
 
 BT_FIRMWARE := btfw32.tlv btnv32.bin
 BT_FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(BT_FIRMWARE)))
