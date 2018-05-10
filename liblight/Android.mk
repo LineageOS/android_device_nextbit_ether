6b5214f8035ef58caa58d40c3b7776d5daab4b70
chrmhoffmann <chrmhoffmann@gmail.com>
2018-05-10 19:24:40 +0200

    ether: move lights hal to vendor
    Change-Id: Ie491cdf4c57097bc1e3d70d45c56beb08c6b656e

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -23,5 +23,6 @@ LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_CFLAGS := $(common_flags) -DLOG_TAG=\"ether-lights\"
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
