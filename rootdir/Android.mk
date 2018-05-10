f90acda90a1183a557c32d0a3af4ba7dfd3a53e8
chrmhoffmann <chrmhoffmann@gmail.com>
2018-05-10 11:52:46 +0200

    Revert "ether: Allow overriding usb init script with bullhead's"
    This reverts commit e58a2c2b43a94f635f2de638ab65d5d5a66c967a.
    
    * It is not clear why this would help nor what the problem is.
    
    Change-Id: I4dbb477ad5eca974c240561466666ab3d41d13dd

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -46,11 +46,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.nbq.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-ifeq ($(TARGET_NEEDS_HACKED_VENDORID), true)
-LOCAL_SRC_FILES    := init.bullhead.usb.rc
-else
 LOCAL_SRC_FILES    := init.nbq.usb.rc
-endif
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
