ab041ec3d749bf3a2aabd8ac1021463382b258c5
chrmhoffmann <chrmhoffmann@gmail.com>
2018-05-11 15:18:21 +0200

    ether: move camera hal to /vendor
    Change-Id: I9e39fe7260442822d7ab5cb633f17ed3814e5be3

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -22,6 +22,7 @@ LOCAL_SRC_FILES := qomx_core.c
 
 LOCAL_MODULE           := libqomx_core
 LOCAL_SHARED_LIBRARIES := libcutils libdl liblog
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
