9c261dd4780bd12aac4fd3ee6dae96d40ac4a9bb
chrmhoffmann <chrmhoffmann@gmail.com>
2018-05-11 14:45:25 +0200

    ether: move nfc config files to /vendor
    Change-Id: Ic9ce87967d30ad41ff5b8529b3f800f6582cbb07

--- device.mk
+++ device.mk
@@ -187,8 +187,8 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # Permissions
