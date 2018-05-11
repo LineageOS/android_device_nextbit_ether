907440b729bf744a83cab4693d2160fbfcb9ab09
chrmhoffmann <chrmhoffmann@gmail.com>
2018-05-11 19:03:11 +0200

    ether: selinux fixes for O
    Change-Id: I7f97a696ce642bcc340e3eae442ce867af18ca02

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -92,10 +92,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.nbq.power.sh
+LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.nbq.power.sh
+LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
