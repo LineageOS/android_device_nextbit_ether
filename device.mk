458b084050cfe67819996a23ccad6dbd049c854b
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:17:21 +0300

    Revert "ether: Nuke irqbalance"
    * There is no real reason to remove msm_irqbalance.
      If proper research shows it's harmful, we can remove it again.
      Until then, keep it like every other device.
    
    This reverts commit 5af91ec331916314082473de6a6b423cb013f28d.
    
    Change-Id: I1d3d92d7070ecfd7cc947f7ea0d363494a06046e

--- device.mk
+++ device.mk
@@ -173,7 +173,8 @@ PRODUCT_COPY_FILES += \
 
 # Misc
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/etc/msm_irqbalance.conf
 
 PRODUCT_PACKAGES += \
     libtinyxml
