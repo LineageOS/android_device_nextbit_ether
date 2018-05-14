2c9c2342b6e83efe52d4476649449d5c357356e6
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:03:31 +0300

    ether: Remove custom power HAL lib
    * Removal of input boost causes lag.
      No need to deviate from standard HAL that has been tested for years
    
    Revert "ether: power: Cleanup custom power lib"
    
    This reverts commit 4c5db0993a4eff79d5bdcd1a63ea95ac3b2b655e.
    
    Revert "ether: power: Remove all hints of boosting"
    
    This reverts commit 3963825ac66863323f4abc65e562342d8e63ed68.
    
    Revert "ether: power: Add custom handling of decode hint"
    
    This reverts commit 059f55682460860da89822f730ced51484c93669.
    
    Revert "ether: Implement custom power hal lib"
    
    This reverts commit 53f41312396257ca8a99ef3e9a3f42c93cfcfc5e.
    
    Change-Id: I0a7609d70443b9ee70515ce4771308b94c1a8921

--- power/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := hardware/qcom/power
-LOCAL_CFLAGS := -Wall -Werror
-LOCAL_SRC_FILES := power-ether.c
-LOCAL_MODULE := libpower_ether
-include $(BUILD_STATIC_LIBRARY)
