71a8f16cc5c96927e614520d557370d2fe10ffdd
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 23:40:32 +0300

    Revert "ether: Move perf profile configs from vendor"
    * We don't need to touch them
    
    This reverts commit 4a9f8a2037730fdc40343826c47b664c9ad656bc.
    
    Change-Id: I90456c19ddc27e6b4603684a90581797b8aa3416

--- device.mk
+++ device.mk
@@ -227,14 +227,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service-qti \
     init.nbq.power.sh
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf-profiles/perf-profile1.conf:system/vendor/etc/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/perf-profiles/perf-profile1.conf:system/vendor/etc/perf-profile1.conf \
-    $(LOCAL_PATH)/configs/perf-profiles/perf-profile2.conf:system/vendor/etc/perf-profile2.conf \
-    $(LOCAL_PATH)/configs/perf-profiles/perf-profile3.conf:system/vendor/etc/perf-profile3.conf \
-    $(LOCAL_PATH)/configs/perf-profiles/perf-profile4.conf:system/vendor/etc/perf-profile4.conf \
-    $(LOCAL_PATH)/configs/perf-profiles/perf-profile5.conf:system/vendor/etc/perf-profile5.conf
-
 # Radio
 PRODUCT_PACKAGES += \
     ebtables \
