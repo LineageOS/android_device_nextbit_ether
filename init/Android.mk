e4080e5797bc3df9fe706bd96d4c9c1c6edd6c50
Michael Bestas <mkbestas@lineageos.org>
2018-05-16 00:23:38 +0300

    ether: init: Cleanup and fix code style
    Change-Id: Ice339617a0fb4555b5f37f42084ceaf98535bd24

--- init/Android.mk
+++ init/Android.mk
@@ -3,11 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES :=  system/core/init
-LOCAL_CFLAGS := -Wall
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+LOCAL_CPPFLAGS := -Wall
 LOCAL_SRC_FILES := init_ether.cpp
 LOCAL_MODULE := libinit_ether
-
-LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
