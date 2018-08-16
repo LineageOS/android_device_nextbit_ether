9293defb4b9619bdd0354006de007cca8dd6d35b
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    ether: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -258,7 +258,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
