7890aa5d4c7bcca1596008a5d85cb0b709bb34cf
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    ether: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- device.mk
+++ device.mk
@@ -233,7 +233,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti \
+    android.hardware.power@1.1-service-qti \
     init.nbq.power.sh
 
 # Radio
