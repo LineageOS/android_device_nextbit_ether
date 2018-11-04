4c7ca979673c809ad32238f0c75945a9f55341fc
Oskar Kratochvil <oskarkratochvil86@yahoo.com>
2018-11-04 13:22:57 +0200

    [DNM] ether: remove ANT+
    Change-Id: I8969d2b5e675003e050f2a31b336fb625c2cd41e

--- device.mk
+++ device.mk
@@ -24,12 +24,6 @@ DEVICE_PACKAGE_OVERLAYS := \
 
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
@@ -203,7 +197,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
