6fa0bb237a75600250061a40a9ac7a09b9c2c983
Paul Keith <javelinanddart@gmail.com>
2019-01-18 23:36:25 +0100

    ether: Build livedisplay 2.0 service
    * Only IPictureAdjustment is added because only that is supported
    
    Change-Id: Ia96dfe341d07c9f79b1ab5bd19af0e32bee4b4ed

--- device.mk
+++ device.mk
@@ -157,7 +157,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
+    vendor.lineage.livedisplay@1.0-service-legacymm \
+    vendor.lineage.livedisplay@2.0-service-qti
 
 # Media
 PRODUCT_PACKAGES += \
