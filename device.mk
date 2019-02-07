a97acfd30c39ad810758a02ab50535c9c4898e9d
Paul Keith <javelinanddart@gmail.com>
2019-02-07 13:12:30 -0600

    ether: Build livedisplay sysfs service
    * Add supported interfaces to manifest while we're at it
    
    Change-Id: Iaad13fcdfb603d144599133339f9c0c6a0d1c66a

--- device.mk
+++ device.mk
@@ -157,7 +157,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-legacymm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media
 PRODUCT_PACKAGES += \
