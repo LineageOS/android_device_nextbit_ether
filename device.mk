5d081915629fe73c5cdd603d96726de6c88da739
Wang Han <416810799@qq.com>
2019-09-06 20:23:17 +0800

    ether: Use XML audio policy configuration
     * Based on hardware/qcom/audio, with FM support removed.
    
    Change-Id: Iaab78dd6bdadb1916e071152ead1a157165b8496
    (cherry picked from commit 21336613f2fa955b8490efa711309f0cecbd52b5)

--- device.mk
+++ device.mk
@@ -48,7 +48,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
@@ -60,7 +60,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdbdata/NBQ_Handset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Handset_cal.acdb \
     $(LOCAL_PATH)/audio/acdbdata/NBQ_Hdmi_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Hdmi_cal.acdb \
     $(LOCAL_PATH)/audio/acdbdata/NBQ_Headset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Headset_cal.acdb \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_Speaker_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Speaker_cal.acdb
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_Speaker_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Speaker_cal.acdb \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
