e4e30833d4df88ab5c9239deca6f26f1fc542650
bgcngm <bgcngm@gmail.com>
2019-10-27 18:43:19 +0000

    ether: Drop mediaextractor seccomp policy
    * These syscalls are now allowed by default.
    
    * [arco] Also clean mediacodec.policy
    
    Change-Id: I21592135dfba78886ea17d7c95bac17778d60b59

--- device.mk
+++ device.mk
@@ -256,8 +256,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
