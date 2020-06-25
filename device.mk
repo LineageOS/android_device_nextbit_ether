731be7a484a64a397f7ac22a66c6705c3335139c
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-25 11:38:06 -0400

    ether: Move ACDB to vendor
    Change-Id: Ie0f0b3e513b52e6f0150779b4e6cd1335be75932

--- device.mk
+++ device.mk
@@ -50,13 +50,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Bluetooth_cal.acdb \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_General_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_General_cal.acdb \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_Global_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Global_cal.acdb \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_Handset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Handset_cal.acdb \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_Hdmi_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Hdmi_cal.acdb \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_Headset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Headset_cal.acdb \
-    $(LOCAL_PATH)/audio/acdbdata/NBQ_Speaker_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/NBQ/NBQ_Speaker_cal.acdb \
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/NBQ/NBQ_Bluetooth_cal.acdb \
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/NBQ/NBQ_General_cal.acdb \
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/NBQ/NBQ_Global_cal.acdb \
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/NBQ/NBQ_Handset_cal.acdb \
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/NBQ/NBQ_Hdmi_cal.acdb \
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/NBQ/NBQ_Headset_cal.acdb \
+    $(LOCAL_PATH)/audio/acdbdata/NBQ_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/NBQ/NBQ_Speaker_cal.acdb \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
