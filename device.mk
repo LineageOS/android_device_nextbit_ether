91cb5cc8c5cc7f51294037acd155bf85bfabaac3
Michael Bestas <mkbestas@lineageos.org>
2020-06-25 11:54:23 -0400

    ether: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- device.mk
+++ device.mk
@@ -236,7 +236,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti \
+    android.hardware.power@1.2-service-qti \
     init.nbq.power.sh
 
 # Radio
