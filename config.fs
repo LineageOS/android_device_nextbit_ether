2a2c615db18fea80ece4b9e0004cc7c7f3469ed4
Bruno Martins <bgcngm@gmail.com>
2020-06-24 22:55:21 -0400

    ether: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- config.fs
+++ config.fs
@@ -7,6 +7,24 @@ value:2951
 [AID_VENDOR_RFS_SHARED]
 value:2952
 
+[bt_firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
 [vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
