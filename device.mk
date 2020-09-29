38de3880504bc0d2b1455d2239746d27f6e4946f
Bruno Martins <bgcngm@gmail.com>
2020-09-29 10:45:23 +0100

    ether: Build health HAL as per R requirements
    The devices fail to boot otherwise.
    
    Change-Id: Ia245ace574e3079d47287cd0e5ec908771851035

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/izat.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/izat.conf \
     $(LOCAL_PATH)/gps/sap.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sap.conf
 
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service
+
 # HIDL
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
