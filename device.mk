aeaa79e2bf24f67cd079a37a9689ffb4cc8c360b
Nolen Johnson <johnsonnolen@gmail.com>
2020-11-28 16:03:32 -0700

    ether: Use libprotobuf-cpp-full-v29.so for libsettings.so
    * The symbol `_ZN6google8protobuf8internal13empty_string_E` was
      removed in the API30 version of this lib. Copy the old one
      and patchelf in the dependency.
    
    Change-Id: I3553c9cbb6ccff202aab706b7b7fa2f39d1480a9

--- device.mk
+++ device.mk
@@ -315,7 +315,9 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
+
 
 # WiFi
 PRODUCT_PACKAGES += \
